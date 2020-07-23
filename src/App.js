250cb95f8759fd44829b4a4750598b4c772fd901
lukecheng1998 <lukecheng1998@gmail.com>
2020-07-23 11:30:56 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {Provider} from "react-redux";
+import jwtDecode from 'jwt-decode';
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme"
+//import theme file
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
